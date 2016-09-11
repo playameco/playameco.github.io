b39ab34ef2dc6e83a048f9181325feaf8b59fbcb
gwenf <gwenfaraday@gmail.com>
2016-09-11 15:03:07 -0400

    point system

--- src/components/PointSystem.js
+++ src/components/PointSystem.js
@@ -6,7 +6,11 @@ class PointSystem extends Component {
       <div className='point-system'>
         <h1>Point System</h1>
         <p>Earn points for learning and participating in recycling, reusing, and reducing waste.</p>
-        
+        <ol>
+        	<li>Sign up for an account.</li>
+        	<li>Watch tutorials to earn points.</li>
+        	<li>Earn even more points by recycling and reusing materials.</li>
+        </ol>
       </div>
     );
   }
