37eb26d7a0c988e2229a56c770514a5033f4d31d
gwenf <gwenfaraday@gmail.com>
2016-09-11 11:53:30 -0400

    first commit

--- src/components/Materials/ReuseSection.js
+++ src/components/Materials/ReuseSection.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import FontAwesome from 'react-fontawesome'
 
 class ReuseSection extends Component {
   constructor(){
@@ -18,10 +19,57 @@ class ReuseSection extends Component {
     return (
       <div className='sections'>
         <h1>Ideas for Reusing {this.state.material.replace(this.state.material[0], this.state.material[0].toUpperCase())}</h1>
-        <div className='social-media-hashtag'></div>
-        <div className='social-media-hashtag'></div>
-        <ul>
-          <li>List of ways to reuse {this.state.material}...</li>
+        <div className='social-media-hashtag'>#amEco</div>
+        <div className='social-media-hashtag'>#{this.state.material}</div>
+        <ul className='social-feed-list'>
+          <li className='social-feed-item'>
+            <div className='dummy-image'></div>
+            <div className='social-text-area'>
+              <h3>Amy Knight</h3>
+              <p>
+                Look at how I reused this <strong>#{this.state.material}</strong>. 
+                Why throw stuff away, when it can be this beatiful? <strong>#emEco</strong>
+              </p>
+              <i className="fa fa-retweet" aria-hidden="true"></i> 134
+              <i className="fa fa-heart" aria-hidden="true"></i> 173
+            </div>
+          </li>
+          <li className='social-feed-item'>
+            <div className='dummy-image'></div>
+            <div className='social-text-area'>
+              <h3>Gwen Faraday</h3>
+              <p>
+                Look at how I reused this <strong>#{this.state.material}</strong>. 
+                Why throw stuff away, when it can be this beatiful? <strong>#emEco</strong>
+              </p>
+              <i className="fa fa-retweet" aria-hidden="true"></i> 20
+              <i className="fa fa-heart" aria-hidden="true"></i> 55
+            </div>
+          </li>
+          <li className='social-feed-item'>
+            <div className='dummy-image'></div>
+            <div className='social-text-area'>
+              <h3>Jared Wilcurt</h3>
+              <p>
+                Look at how I reused this <strong>#{this.state.material}</strong>. 
+                Why throw stuff away, when it can be this beatiful? <strong>#emEco</strong>
+              </p>
+              <i className="fa fa-retweet" aria-hidden="true"></i> 12
+              <i className="fa fa-heart" aria-hidden="true"></i> 23
+            </div>
+          </li>
+          <li className='social-feed-item'>
+            <div className='dummy-image'></div>
+            <div className='social-text-area'>
+              <h3>Kim Sharpe</h3>
+              <p>
+                Look at how I reused this <strong>#{this.state.material}</strong>. 
+                Why throw stuff away, when it can be this beatiful? <strong>#emEco</strong>
+              </p>
+              <i className="fa fa-retweet" aria-hidden="true"></i> 4
+              <i className="fa fa-heart" aria-hidden="true"></i> 11
+            </div>
+          </li>
         </ul>
       </div>
     );
