1c42bd8b149364e965d2835f0a0cc2f40c66e217
gwenf <gwenfaraday@gmail.com>
2016-09-11 15:23:26 -0400

    point system updates

--- src/components/PointSystem.js
+++ src/components/PointSystem.js
@@ -5,12 +5,44 @@ class PointSystem extends Component {
     return (
       <div className='point-system'>
         <h1>Point System</h1>
-        <p>Earn points for learning and participating in recycling, reusing, and reducing waste.</p>
-        <ol>
-        	<li>Sign up for an account.</li>
-        	<li>Watch tutorials to earn points.</li>
-        	<li>Earn even more points by recycling and reusing materials.</li>
-        </ol>
+        <ul className="lc-checks">
+		  <li>
+		    <span className="lc-checks__check">✔</span>
+		    <span className="lc-checks__feature">Sign Up</span>
+		  </li>
+		  <li>
+		    <span className="lc-checks__check">✔</span>
+		    <span className="lc-checks__feature">Learn About Recycling</span>
+		  </li>
+		  <li>
+		    <span className="lc-checks__check">✔</span>
+		    <span className="lc-checks__feature">Earn Points!</span>
+		  </li>
+		  <li>
+		    <span className="lc-checks__check">✔</span>
+		    <span className="lc-checks__feature">Recycle</span>
+		  </li>
+		  <li>
+		    <span className="lc-checks__check">✔</span>
+		    <span className="lc-checks__feature">Earn More Points!!</span>
+		  </li>
+		  <li>
+		    <span className="lc-checks__check">✔</span>
+		    <span className="lc-checks__feature">Reuse/Reduce Waste</span>
+		  </li>
+		  <li>
+		    <span className="lc-checks__check">✔</span>
+		    <span className="lc-checks__feature">Earn Even More Points!!!</span>
+		  </li>
+		  <li>
+		    <span className="lc-checks__check">✔</span>
+		    <span className="lc-checks__feature">Collect Cute Animals</span>
+		  </li>
+		  <li>
+		    <span className="lc-checks__check">✔</span>
+		    <span className="lc-checks__feature">Encourage Others to Join You</span>
+		  </li>
+		</ul>
       </div>
     );
   }
